0c4dd753bcea639d978bd16c027086199ad4233c
Anna Bell <undefinedvalue@users.noreply.github.com>
2024-06-28 13:15:26 -0700

    Move Communicator to ConsoleRunner

---
+++ Source/ConsoleRunner/Communicator.cs
@@ -0,0 +1,24 @@
+﻿using Core;
+using Lib;
+
+namespace ConsoleRunner;
+
+public static class Communicator
+{
+    private const string ENDPOINT = "https://boundvariable.space/communicate";
+    private const string KEY = "ff993058-d102-4ee8-913f-e1c39614b957";
+
+    public static string SendRaw(string icfp)
+    {
+        RestConnector.SetKey(KEY);
+        return RestConnector.PostFile(ENDPOINT, icfp);
+    }
+
+    public static string Send(string message)
+    {
+        // TODO: message should be human readable. Convert to ICFP.
+        string icfp = message;
+        string icfpReply = SendRaw(icfp);
+        return Expression.Parse(icfpReply).ToString() ?? "";
+    }
+}
