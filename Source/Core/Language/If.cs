43ea57feb463fbdc4334c57acb18cc9f641bf3c0
William Pittman <wspittman@gmail.com>
2024-06-28 13:02:22 -0700

    Core: Add If expression

---
+++ Source/Core/Language/If.cs
@@ -0,0 +1,34 @@
+﻿namespace Core;
+
+public class If : Expression
+{
+    public Expression Condition { get; }
+    public Expression Then { get; }
+    public Expression Else { get; }
+
+    public If(Expression condition, Expression then, Expression @else)
+    {
+        Condition = condition;
+        Then = then;
+        Else = @else;
+    }
+
+    public Value Eval(Dictionary<int, Value> environment)
+    {
+        var condition = Condition.Eval(environment);
+
+        if (condition.AsBool())
+        {
+            return Then.Eval(environment);
+        }
+        else
+        {
+            return Else.Eval(environment);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"(if {Condition} {Then} {Else})";
+    }
+}
