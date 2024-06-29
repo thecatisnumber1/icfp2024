41686faba85b76cc3b715049ca843f050392e678
Anna Bell <undefinedvalue@users.noreply.github.com>
2024-06-28 18:58:57 -0700

    Improvements to program

--- Source/ConsoleRunner/Program.cs
+++ Source/ConsoleRunner/Program.cs
@@ -10,123 +10,115 @@ if (args.Length == 0)
     Console.WriteLine("");
     Console.Write("> ");
 
+    Finder tasksFinder = new(Finder.GIT.GetRelativeDir("Tasks").FullName);
+
     string? line;
     while ((line = Console.ReadLine()) != null)
     {
         line = line.Trim();
+        string cmd = line;
 
-        if (line.StartsWith("send "))
-        {
-            string msg = line[5..^0].Trim();
-
-            if (msg != "")
-            {
-                string icfp = Unparse(msg);
-                Console.WriteLine("Sending: " + icfp);
-                string? icfpReply = Communicator.Send(icfp);
-
-                if (icfpReply == null)
-                {
-                    continue;
-                }
-
-                Console.WriteLine("-----Begin raw reply-----");
-                Console.WriteLine(icfpReply);
-                Console.WriteLine("-----End raw reply-----");
-                Console.WriteLine("-----Begin reply-----");
-                Console.WriteLine(Expression.Parse(icfpReply).Eval([]).ToString() ?? "");
-                Console.WriteLine("-----End reply-----");
-            }
-        }
-        else if (line.StartsWith("send-raw "))
-        {
-            string msg = line[9..^0].Trim();
-
-            if (msg != "")
-            {
-                string? icfpReply = Communicator.Send(msg);
-
-                if (icfpReply == null)
-                {
-                    continue;
-                }
-
-                Console.WriteLine("-----Begin raw reply-----");
-                Console.WriteLine(icfpReply);
-                Console.WriteLine("-----End raw reply-----");
-                Console.WriteLine("-----Begin reply-----");
-                Console.WriteLine(Expression.Parse(icfpReply).Eval([]).ToString() ?? "");
-                Console.WriteLine("-----End reply-----");
-            }
-        }
-        else if (line.StartsWith("encode "))
-        {
-            string msg = line[7..^0].Trim();
-
-            if (msg != "")
-            {
-                Console.WriteLine(Unparse(msg));
-            }
-        }
-        else if (line.StartsWith("decode "))
-        {
-            string msg = line[7..^0].Trim();
-
-            if (msg != "")
-            {
-                Console.WriteLine(Expression.Parse(msg).Eval([]).ToString());
-            }
-        }
-        else if (line.StartsWith("download "))
-        {
-            string taskName = line[8..^0].Trim();
-
-            if (taskName != "")
-            {
-                TaskDownloader.Download(taskName);
-            }
-        }
-        else if (line.StartsWith("strings "))
-        {
-            string msg = line[6..^0].Trim();
-
-            if (msg != "")
-            {
-                foreach (string token in msg.Split(' '))
-                {
-                    if (token.StartsWith('S') && token.Length > 1)
-                    {
-                        Console.WriteLine(new Str(token).ToString());
-                    }
-                }
-            }
-        }
-        else if (line.StartsWith("strings-file "))
-        {
-            string filename = line[12..^0].Trim();
-
-            if (filename != "")
-            {
-                var file = Finder.GIT.GetRelativeFile(filename);
-
-                if (!file.Exists)
-                {
-                    Console.WriteLine("No such file: " + filename);
-                    continue;
-                }
-
-                foreach (string token in File.ReadAllText(file.FullName).Split(' '))
-                {
-                    if (token.StartsWith('S') && token.Length > 1)
-                    {
-                        Console.WriteLine(new Str(token[1..^0]).ToString());
-                    }
-                }
-            }
-        }
-        else if(line == "exit")
+        if (cmd.StartsWith("exit"))
         {
             return;
+        }
+
+        int spaceIdx = line.IndexOf(' ');
+        string cmdargs;
+
+        if (spaceIdx > 0)
+        {
+            cmd = line[0..spaceIdx];
+            cmdargs = line[(spaceIdx + 1)..].Trim();
+
+            string[] splitArgs = cmdargs.Split(' ');
+
+            // If the args are just "< file.txt", read args from that file
+            if (splitArgs.Length == 2 && splitArgs[0] == "<")
+            {
+                // Search for the file recursively in Tasks
+                var file = tasksFinder.FindFile(splitArgs[1]);
+
+                if (file == null)
+                {
+                    Console.WriteLine("No such file in Tasks: " + splitArgs[1]);
+                    continue;
+                }
+
+                cmdargs = File.ReadAllText(file.FullName);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Enter command input, end with \"EOF\" on its own line");
+            Console.WriteLine("-----");
+
+            cmdargs = "";
+
+            while ((line = Console.ReadLine())?.Trim() != "EOF")
+            {
+                cmdargs += line + "\n";
+            }
+
+            // Remove last newline
+            cmdargs = cmdargs[..^1];
+        }
+
+        if (cmd == "send")
+        {
+            string icfp = Unparse(cmdargs);
+            Console.WriteLine("Sending: " + icfp);
+            string? icfpReply = Communicator.Send(icfp);
+
+            if (icfpReply == null)
+            {
+                continue;
+            }
+
+            Console.WriteLine("-----Begin raw reply-----");
+            Console.WriteLine(icfpReply);
+            Console.WriteLine("-----End raw reply-----");
+            Console.WriteLine("-----Begin reply-----");
+            Console.WriteLine(Expression.Parse(icfpReply).Eval([]).ToString() ?? "");
+            Console.WriteLine("-----End reply-----");
+        }
+        else if (cmd == "send-raw")
+        {
+            string? icfpReply = Communicator.Send(cmdargs);
+
+            if (icfpReply == null)
+            {
+                continue;
+            }
+
+            Console.WriteLine("-----Begin raw reply-----");
+            Console.WriteLine(icfpReply);
+            Console.WriteLine("-----End raw reply-----");
+            Console.WriteLine("-----Begin reply-----");
+            Console.WriteLine(Expression.Parse(icfpReply).Eval([]).ToString() ?? "");
+            Console.WriteLine("-----End reply-----");
+        }
+        else if (cmd == "encode")
+        {
+            Console.WriteLine(Unparse(cmdargs));
+        }
+        else if (cmd == "decode")
+        {
+            Console.WriteLine(Expression.Parse(cmdargs).Eval([]).ToString());
+        }
+        else if (cmd == "download")
+        {
+            TaskDownloader.Download(cmdargs);
+        }
+        else if (cmd == "strings")
+        {
+            foreach (string token in cmdargs.Split(' '))
+            {
+                if (token.StartsWith('S') && token.Length > 1)
+                {
+                    Console.WriteLine(new Str(token[1..]).ToString());
+                }
+            }
         }
         else
         {
@@ -138,8 +130,15 @@ if (args.Length == 0)
                 decode <icfp>        Decode an ICFP string
                 download <taskname>  Downloads a task and its problems
                 strings <icfp>       Outputs all strings in the ICFP
-                strings-file <file>  Same as strings but loads ICFP from a file
                 exit                 Exit
+
+                Any command run without an argument will enter input mode, which is
+                useful for entering multiline content.
+
+                Any command accepting an argument can also accept a file by using <
+                decode < spaceship1-raw.txt
+
+                All such files are searched for recursively in the Tasks directory.
             """);
         }
 
